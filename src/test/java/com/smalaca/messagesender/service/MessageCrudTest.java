c635fa60504e888439095feac02823c3a0d97b75
smalaca <ab13.krakow@gmail.com>
2018-02-18 08:20:22 +0100

    Use mock instead of real implementation

--- src/test/java/com/smalaca/messagesender/service/MessageCrudTest.java
+++ src/test/java/com/smalaca/messagesender/service/MessageCrudTest.java
@@ -5,47 +5,53 @@ import com.smalaca.messagesender.domain.MessageRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.hamcrest.CoreMatchers.any;
+import static org.mockito.Matchers.isA;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("/message-sender.xml")
+@ContextConfiguration(locations = {"/message-sender.xml", "/mocked-repositories.xml"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class MessageCrudTest {
+    private static final String MESSAGE_FROM = "from";
+    private static final String MESSAGE_TO = "to";
+    private static final String BODY = "body";
+    private static final String SUBJECT = "subject";
     @Autowired private MessageRepository messageRepository;
     @Autowired private MessageCrud messageCrud;
 
     @Test
     public void shouldCreateNewMessage() {
-        String subject = "subject";
-        String body = "body";
-        String from = "from";
-        String to = "to";
-
-        Response response = messageCrud.createNew(subject, body, from, to);
+        Response response = messageCrud.createNew(SUBJECT, BODY, MESSAGE_FROM, MESSAGE_TO);
 
         Assert.assertTrue(response.isSuccess());
         Assert.assertThat(response.getMessage(), any(String.class));
-        Assert.assertTrue(messageRepository.exists(response.getMessage()));
+        ArgumentCaptor<Message> messageArgumentCaptor  = ArgumentCaptor.forClass(Message.class);
+        BDDMockito.then(messageRepository).should().add(messageArgumentCaptor.capture());
+        assertCreatedMessage(messageArgumentCaptor.getValue());
+    }
+
+    private void assertCreatedMessage(Message message) {
+        Assert.assertEquals(SUBJECT, message.getSubject());
+        Assert.assertEquals(BODY, message.getBody());
+        Assert.assertEquals(MESSAGE_FROM, message.getFrom());
+        Assert.assertEquals(MESSAGE_TO, message.getTo());
     }
 
     @Test
     public void shouldNotCreateNewMessage() {
-        String subject = "subject";
-        String body = "body";
-        String from = "from";
-        String to = "to";
-        messageRepository.add(new Message.MessageBuilder()
-                .withBody(body)
-                .withSubject(subject)
-                .withFrom(from)
-                .withTo(to)
-                .build());
+        BDDMockito.given(messageRepository.exists(isA(Message.class))).willReturn(true);
 
-        Response response = messageCrud.createNew(subject, body, from, to);
+        Response response = messageCrud.createNew(SUBJECT, BODY, MESSAGE_FROM, MESSAGE_TO);
 
         Assert.assertFalse(response.isSuccess());
+        BDDMockito.then(messageRepository).should(Mockito.never()).add(isA(Message.class));
     }
 }
