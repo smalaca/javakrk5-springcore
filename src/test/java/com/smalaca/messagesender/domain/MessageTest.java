72e55dbcd4855bdc3cc93373b70609c05cedf171
smalaca <ab13.krakow@gmail.com>
2018-02-18 11:06:20 +0100

    Remove usage of the MessageBuilder from tests

--- src/test/java/com/smalaca/messagesender/domain/MessageTest.java
+++ src/test/java/com/smalaca/messagesender/domain/MessageTest.java
@@ -1,26 +1,23 @@
 package com.smalaca.messagesender.domain;
 
+import com.smalaca.messagesender.service.MessageDto;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MessageTest {
     @Test
     public void shouldCreateMessageWithRequiredFields() {
-        String sentTo = "to";
-        String sentFrom = "from";
-        String body = "body";
-        String subject = "subject";
+        MessageDto messageDto = new MessageDto();
+        messageDto.setSubject("subject");
+        messageDto.setBody("body");
+        messageDto.setFrom("from");
+        messageDto.setTo("to");
 
-        Message message = new Message.MessageBuilder()
-                .withSubject(subject)
-                .withBody(body)
-                .withFrom(sentFrom)
-                .withTo(sentTo)
-                .build();
+        Message message = new MessageFactory().createFrom(messageDto);
 
-        Assert.assertEquals(subject, message.getSubject());
-        Assert.assertEquals(body, message.getBody());
-        Assert.assertEquals(sentFrom, message.getFrom());
-        Assert.assertEquals(sentTo, message.getTo());
+        Assert.assertEquals(messageDto.getSubject(), message.getSubject());
+        Assert.assertEquals(messageDto.getBody(), message.getBody());
+        Assert.assertEquals(messageDto.getFrom(), message.getFrom());
+        Assert.assertEquals(messageDto.getTo(), message.getTo());
     }
 }
