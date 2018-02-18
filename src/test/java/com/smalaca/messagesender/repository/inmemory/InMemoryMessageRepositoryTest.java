26207ca166fe4c7c1ea9a561d52f26d8ff20621c
smalaca <ab13.krakow@gmail.com>
2018-02-18 12:55:35 +0100

    Replace creation of Message by new with MessageFactory in InMemoryMessageRepositoryTest

--- src/test/java/com/smalaca/messagesender/repository/inmemory/InMemoryMessageRepositoryTest.java
+++ src/test/java/com/smalaca/messagesender/repository/inmemory/InMemoryMessageRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.smalaca.messagesender.repository.inmemory;
 
 import com.smalaca.messagesender.domain.Message;
+import com.smalaca.messagesender.domain.MessageFactory;
+import com.smalaca.messagesender.service.MessageDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,62 +15,72 @@ import static org.junit.Assert.*;
 @ContextConfiguration("/repositories.xml")
 public class InMemoryMessageRepositoryTest {
     @Autowired private InMemoryMessageRepository repository;
+    private MessageFactory factory = new MessageFactory();
 
     @Test
     public void shouldRecognizeThatFirstMessageExist() {
-        Message message = new Message("some subject", "some body");
+        MessageDto messageDto = new MessageDto();
+        messageDto.setSubject("some subject");
+        messageDto.setBody("some body");
 
-        assertTrue(repository.exists(message));
+        assertTrue(repository.exists(factory.createFrom(messageDto)));
     }
 
     @Test
-    public void shouldRecognizeThatMessageSimilarToFirstDoeasExist() {
-        Message message = new Message("some subject", "some body");
-        message.setId("some id");
+    public void shouldRecognizeThatMessageSimilarToFirstDoesNotExist() {
+        MessageDto messageDto = new MessageDto();
+        messageDto.setSubject("some subject");
+        messageDto.setBody("some body");
 
-        assertFalse(repository.exists(message));
+        assertFalse(repository.exists(factory.createFrom(messageDto, "some id")));
     }
 
     @Test
     public void shouldRecognizeThatSecodeMessageExist() {
-        Message message = new Message("some", "some");
-        message.setId("123456");
+        MessageDto messageDto = new MessageDto();
+        messageDto.setSubject("some");
+        messageDto.setBody("some");
 
-        assertTrue(repository.exists(message));
+        assertTrue(repository.exists(factory.createFrom(messageDto, "123456")));
     }
 
     @Test
-    public void shouldRecognizeThatMessageSimilarToSecondDoeasExist() {
-        Message message = new Message("some", "some");
-        message.setId("1234567");
+    public void shouldRecognizeThatMessageSimilarToSecondDoesNotExist() {
+        MessageDto messageDto = new MessageDto();
+        messageDto.setSubject("some");
+        messageDto.setBody("some");
 
-        assertFalse(repository.exists(message));
+        assertFalse(repository.exists(factory.createFrom(messageDto, "1234567")));
     }
 
     @Test
     public void shouldRecognizeThatThirdMessageExist() {
-        Message message = new Message("some subject 2", "some body 2");
-        message.setId("123456");
-        message.setTo("javakrk5");
-        message.setFrom("smalaca");
+        MessageDto messageDto = new MessageDto();
+        messageDto.setSubject("some subject 2");
+        messageDto.setBody("some body 2");
+        messageDto.setTo("javakrk5");
+        messageDto.setFrom("smalaca");
 
-        assertTrue(repository.exists(message));
+        assertTrue(repository.exists(factory.createFrom(messageDto, "123456")));
     }
 
     @Test
     public void shouldRecognizeThatFourthMessageExist() {
-        Message message = new Message("hot topic", "nice body");
+        MessageDto messageDto = new MessageDto();
+        messageDto.setSubject("hot topic");
+        messageDto.setBody("nice body");
 
-        assertTrue(repository.exists(message));
+        assertTrue(repository.exists(factory.createFrom(messageDto)));
     }
 
     @Test
-    public void shouldRecognizeThatMessageSimilarToThirdDoeasExist() {
-        Message message = new Message("some subject 2", "some body 2");
-        message.setId("123456");
-        message.setTo("smalaca");
-        message.setFrom("javakrk5");
+    public void shouldRecognizeThatMessageSimilarToThirdDoesNotExist() {
+        MessageDto messageDto = new MessageDto();
+        messageDto.setSubject("some subject 2");
+        messageDto.setBody("some body 2");
+        messageDto.setTo("smalaca");
+        messageDto.setFrom("javakrk5");
 
-        assertFalse(repository.exists(message));
+        assertFalse(repository.exists(factory.createFrom(messageDto, "123456")));
     }
 }
