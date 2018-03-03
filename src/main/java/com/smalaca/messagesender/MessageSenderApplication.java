e1f0b2793adc5b5a812323ee0e3802ce413258cf
smalaca <ab13.krakow@gmail.com>
2018-03-03 13:53:07 +0100

    start with spring boot

--- src/main/java/com/smalaca/messagesender/MessageSenderApplication.java
+++ src/main/java/com/smalaca/messagesender/MessageSenderApplication.java
@@ -7,27 +7,10 @@ import com.smalaca.messagesender.service.Response;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@SpringBootApplication
+@ImportResource({"classpath*:message-sender.xml"})
 public class MessageSenderApplication {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("message-sender.xml");
-        MessageCrud messageCrud = (MessageCrud)     context.getBean("messageCrud");
-        String subject = "subject";
-        String body = "body";
-        String from = "from";
-        String to = "to";
-
-        MessageDto messageDto = new MessageDto();
-        messageDto.setBody(body);
-        messageDto.setSubject(subject);
-        messageDto.setFrom(from);
-        messageDto.setTo(to);
-
-        Response response = messageCrud.createNew(messageDto);
-        System.out.println(response.isSuccess());
-        System.out.println(response.getMessage());
-
-        response = messageCrud.createNew(messageDto);
-        System.out.println(response.isSuccess());
-        System.out.println(response.getMessage());
+        SpringApplication.run(MessageSenderApplication.class, args);
     }
 }
