848cc94b5e7dde3a1d96981c45dea6b858927598
smalaca <ab13.krakow@gmail.com>
2018-03-03 22:06:22 +0100

    More ways to define rest controller's methods

--- src/main/java/com/smalaca/messagesender/rest/api/MessageCrudController.java
+++ src/main/java/com/smalaca/messagesender/rest/api/MessageCrudController.java
@@ -1,27 +1,50 @@
 package com.smalaca.messagesender.rest.api;
 
-import com.smalaca.messagesender.rest.dto.MessageSubject;
-import com.sun.corba.se.spi.activation._RepositoryStub;
+import com.smalaca.messagesender.service.MessageCrud;
+import com.smalaca.messagesender.service.MessageDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
+@RequestMapping("/message")
 public class MessageCrudController {
+    private final MessageCrud messageCrud;
+
+    @Autowired
+    public MessageCrudController(MessageCrud messageCrud) {
+        this.messageCrud = messageCrud;
+    }
 
     @RequestMapping("/add")
     public String add() {
-        return "added";
+        return "Added";
     }
 
-    @RequestMapping("/delete")
-    public String delete(@RequestParam String uniqueIdentifier, @RequestParam(name = "shortname", defaultValue = "NO_NAME") String name) {
-        return "message with id: " + uniqueIdentifier + " deleted. Hi " + name;
+    @RequestMapping("/get")
+    public String get(@RequestParam String id) {
+        return "Message with id: " + id;
     }
 
-    @RequestMapping("/update")
-    @ResponseBody
-    public String update(@ModelAttribute("subject") MessageSubject subject) {
-        return "subject of the message changed into " + subject.getSubject();
+    @RequestMapping("/find/{id}")
+    public String find(@PathVariable String id) {
+        return "Found message with id: " + id;
+    }
+
+    @RequestMapping("/find-by/{id}")
+    public MessageDto findById(@PathVariable String id) {
+        MessageDto messageDto = new MessageDto();
+        messageDto.setSubject("subject");
+        messageDto.setBody("body");
+        return messageDto;
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    public String delete(@RequestParam(name = "id") String uniqueIdentifier, @RequestParam(defaultValue = "NO_NAME") String name) {
+        return "Hi " + name + ". Message with id: " + uniqueIdentifier + " removed.";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String update(@ModelAttribute("message") MessageDto messageDto) {
+        return "Updated with subject: " + messageDto.getSubject() + ", body: " + messageDto.getBody() + ", from: " + messageDto.getFrom() + ", to: " + messageDto.getTo();
     }
 }
--- src/test/java/com/smalaca/messagesender/rest/api/MessageCrudControllerTest.java
+++ src/test/java/com/smalaca/messagesender/rest/api/MessageCrudControllerTest.java
@@ -1,28 +1,162 @@
 package com.smalaca.messagesender.rest.api;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class MessageCrudControllerTest {
-    @Autowired
-    private MockMvc mvc;
+    private static final String SOME_SUBJECT = "some subject";
+    private static final String SOME_BODY = "fancy package";
+    private static final String MESSAGE_FROM = "from@me";
+    private static final String MESSAGE_TO = "to@you";
+    private static final String DUMMY_VALUE = "dummy";
+
+    @Autowired private MockMvc mvc;
 
     @Test
-    public void shouldCheckIfAdded() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/add").accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string(CoreMatchers.equalTo("added")));
+    public void shouldAddMessage() throws Exception {
+        MockHttpServletResponse result = mvc.perform(
+                MockMvcRequestBuilders.get("/message/add").accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), result.getStatus());
+        assertEquals("Added", result.getContentAsString());
+    }
+
+    @Test
+    public void shouldReadWithIdAsParam() throws Exception {
+        String uniqueIdentifier = "13";
+        MockHttpServletResponse result = mvc.perform(
+                MockMvcRequestBuilders.get("/message/get")
+                        .param("id", uniqueIdentifier)
+                        .accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), result.getStatus());
+        assertEquals("Message with id: " + uniqueIdentifier, result.getContentAsString());
+    }
+
+    @Test
+    public void shouldReadWithIdInPath() throws Exception {
+        String uniqueIdentifier = "13";
+        MockHttpServletResponse result = mvc.perform(
+                MockMvcRequestBuilders.get("/message/find/{id}", uniqueIdentifier)
+                        .accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), result.getStatus());
+        assertEquals("Found message with id: " + uniqueIdentifier, result.getContentAsString());
+    }
+
+    @Test
+    public void shouldReturnMessageById() throws Exception {
+        String uniqueIdentifier = "13";
+        MockHttpServletResponse result = mvc.perform(
+                MockMvcRequestBuilders.get("/message/find-by/{id}", uniqueIdentifier)
+                        .accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), result.getStatus());
+        assertEquals("{\"body\":\"body\",\"subject\":\"subject\",\"from\":\"\",\"to\":\"\"}", result.getContentAsString());
+    }
+
+    @Test
+    public void shouldDeleteWithBothParameters() throws Exception {
+        String name = "smalaca";
+        String uniqueIdentifier = "13";
+        MockHttpServletResponse result = mvc.perform(
+                MockMvcRequestBuilders.delete("/message/delete")
+                        .param("id", uniqueIdentifier)
+                        .param("name", name)
+                        .accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), result.getStatus());
+        assertEquals("Hi " + name + ". Message with id: " + uniqueIdentifier + " removed.", result.getContentAsString());
+    }
+
+    @Test
+    public void shouldDeleteWithOnlyOneParameters() throws Exception {
+        String uniqueIdentifier = "13";
+        MockHttpServletResponse result = mvc.perform(
+                MockMvcRequestBuilders.delete("/message/delete")
+                        .param("id", uniqueIdentifier)
+                        .accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), result.getStatus());
+        assertEquals("Hi NO_NAME. Message with id: " + uniqueIdentifier + " removed.", result.getContentAsString());
+    }
+
+    @Test
+    public void shouldDeleteWithoutParameters() throws Exception {
+        MockHttpServletResponse result = mvc.perform(
+                MockMvcRequestBuilders.delete("/message/delete")
+                        .accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        assertEquals(HttpStatus.BAD_REQUEST.value(), result.getStatus());
+    }
+
+    @Test
+    public void shouldDeleteWithDifferentRequestMethod() throws Exception {
+        String uniqueIdentifier = "13";
+        MockHttpServletResponse result = mvc.perform(
+                MockMvcRequestBuilders.post("/message/delete")
+                        .param("id", uniqueIdentifier)
+                        .accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        assertEquals(HttpStatus.METHOD_NOT_ALLOWED.value(), result.getStatus());
+    }
+
+    @Test
+    public void shouldUpdateWithAllParameters() throws Exception {
+        MockHttpServletResponse result = mvc.perform(MockMvcRequestBuilders.post("/message/update")
+                .param("subject", SOME_SUBJECT)
+                .param("body", SOME_BODY)
+                .param("from", MESSAGE_FROM)
+                .param("to", MESSAGE_TO)
+                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), result.getStatus());
+        assertEquals(
+                "Updated with subject: " + SOME_SUBJECT + ", body: " + SOME_BODY + ", from: " + MESSAGE_FROM +", to: " + MESSAGE_TO,
+                result.getContentAsString());
+    }
+
+    @Test
+    public void shouldUpdateWithSomeParameters() throws Exception {
+        MockHttpServletResponse result = mvc.perform(MockMvcRequestBuilders.post("/message/update")
+                .param("subject", SOME_SUBJECT)
+                .param("body", SOME_BODY)
+                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), result.getStatus());
+        assertEquals(
+                "Updated with subject: " + SOME_SUBJECT + ", body: " + SOME_BODY + ", from: , to: ",
+                result.getContentAsString());
+    }
+
+    @Test
+    public void shouldUpdateWithMoreParameters() throws Exception {
+        MockHttpServletResponse result = mvc.perform(MockMvcRequestBuilders.post("/message/update")
+                .param("subject", SOME_SUBJECT)
+                .param("body", SOME_BODY)
+                .param("something", DUMMY_VALUE)
+                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), result.getStatus());
+        assertEquals(
+                "Updated with subject: " + SOME_SUBJECT + ", body: " + SOME_BODY + ", from: , to: ",
+                result.getContentAsString());
     }
 }
