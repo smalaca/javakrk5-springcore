1aa666e9e90ae52480a78f753008c4a16b49b329
kamil <kbrzoza1994@gmail.com>
2018-03-03 15:24:35 +0100

    added UserCrud

--- src/main/java/com/smalaca/messagesender/MessageSenderApplication.java
+++ src/main/java/com/smalaca/messagesender/MessageSenderApplication.java
@@ -4,7 +4,10 @@ import com.smalaca.messagesender.domain.Message;
 import com.smalaca.messagesender.service.MessageCrud;
 import com.smalaca.messagesender.service.MessageDto;
 import com.smalaca.messagesender.service.Response;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
