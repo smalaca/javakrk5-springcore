2fac53ab078a327849e9db55233b5eb60f85ed6e
Andrzej <andrzej.chudzicki@onet.pl>
2018-03-03 15:20:56 +0100

    User Controller and User Controller Test added

---
+++ src/test/java/com/smalaca/messagesender/controller/UserControllerTest.java
@@ -0,0 +1,31 @@
+package com.smalaca.messagesender.controller;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void shouldCheckIfCreated() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/createUser").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(CoreMatchers.equalTo("valid")));
+    }
+
+
+}
