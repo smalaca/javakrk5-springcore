d72e1d09740272815627c085de57947fbfc70d35
everlitill <ryba.marcin20gmail.com>
2018-03-06 09:36:48 +0100

    MessageSenderControllerTest - fix

--- src/test/java/com/smalaca/messagesender/controller/MessageSenderControllerTest.java
+++ src/test/java/com/smalaca/messagesender/controller/MessageSenderControllerTest.java
@@ -9,11 +9,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -43,9 +38,12 @@ public class MessageSenderControllerTest {
         repository.add(new MessageFactory().createFrom(messageDto, "1"));
         Assert.assertTrue(repository.exists("1"));
 
-        mvc.perform(MockMvcRequestBuilders.get("/send/1").accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn();
+        MockHttpServletResponse response = mvc.perform(
+                MockMvcRequestBuilders.get("/send/1")
+                        .param("id", "1"))
+                .andReturn().getResponse();
 
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals("{\"success\":true,\"message\":\"OK\"}", response.getContentAsString());
     }
 }
