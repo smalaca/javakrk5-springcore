547c03f9929a113f5afa71efaa1f03b4fbc68458
tblazejowski <tomasz.blazejowski@gmail.com>
2018-03-10 14:08:53 +0100

    test with findAll added

--- src/main/java/com/smalaca/messagesender/domain/TwitterStats.java
+++ src/main/java/com/smalaca/messagesender/domain/TwitterStats.java
@@ -1,28 +1,48 @@
 package com.smalaca.messagesender.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.util.Date;
+import java.sql.Date;
+import java.time.LocalDate;
 
 @Entity
 public class TwitterStats {
 
     @Id @GeneratedValue private String id;
 
-    private final String messageId;
-    private final String from;
-    private final String to;
-    private final Date date;
+    private String messageId;
+    @Column(name = "sentFrom")
+    private String from;
+    private String to;
+    private Date date;
+
+    public TwitterStats() {
+    }
 
     public TwitterStats(String messageId, String from, String to) {
         this.messageId = messageId;
         this.from = from;
         this.to = to;
-        this.date = new Date();
+        this.date = Date.valueOf(LocalDate.now());
     }
 
     public String getId() {
         return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TwitterStats that = (TwitterStats) o;
+
+        if (!id.equals(that.id)) return false;
+        if (!messageId.equals(that.messageId)) return false;
+        if (!from.equals(that.from)) return false;
+        if (!to.equals(that.to)) return false;
+        return date.equals(that.date);
     }
 }
--- src/test/java/com/smalaca/messagesender/domain/TwitterStatsRepositoryTest.java
+++ src/test/java/com/smalaca/messagesender/domain/TwitterStatsRepositoryTest.java
@@ -1,27 +1,68 @@
 package com.smalaca.messagesender.domain;
 
-import com.smalaca.messagesender.service.MessageDto;
-import org.junit.Assert;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class TwitterStatsRepositoryTest {
 
     private static final String FROM = "from";
     private static final String TO = "to";
     private static final String SOME_ID = "13";
 
-    @Autowired TwitterStatsRepository twitterStatsRepository;
+    @Autowired
+    TwitterStatsRepository twitterStatsRepository;
 
     @Test
-    public void shouldStatexists(){
-    assertFalse(twitterStatsRepository.exists(SOME_ID));
+    public void shouldCheckIfStatExists() {
+
+        assertFalse(twitterStatsRepository.exists(SOME_ID));
+        assertNull(twitterStatsRepository.findOne(SOME_ID));
+    }
+
+    @Test
+    public void shouldConfirmThreeStatsAreAdded() {
+
+        twitterStatsRepository.save(new TwitterStats(SOME_ID, FROM, TO));
+        twitterStatsRepository.save(new TwitterStats("14", FROM + " 1", TO + " 1"));
+        twitterStatsRepository.save(new TwitterStats("15", FROM + " 2", TO + " 2"));
+
+        List<TwitterStats> twitterStatsList = toList(twitterStatsRepository.findAll());
+        assertEquals(3, twitterStatsList.size());
+
+    }
+
+    @Test
+    public void shouldConfirmThreeStatsAreAddedAndMatch() {
+
+        TwitterStats persisted1 = twitterStatsRepository.save(new TwitterStats(SOME_ID, FROM, TO));
+        TwitterStats persisted2 = twitterStatsRepository.save(new TwitterStats("14", FROM + " 1", TO + " 1"));
+        TwitterStats persisted3 = twitterStatsRepository.save(new TwitterStats("15", FROM + " 2", TO + " 2"));
+
+        Iterator<TwitterStats> twitterStatsList = twitterStatsRepository.findAll().iterator();
+
+        assertTrue(CollectionUtils.contains(twitterStatsList, persisted1));
+        assertTrue(CollectionUtils.contains(twitterStatsList, persisted2));
+        assertTrue(CollectionUtils.contains(twitterStatsList, persisted3));
+    }
+
+    public static List<TwitterStats> toList(final Iterable<TwitterStats> iterable) {
+        return StreamSupport.stream(iterable.spliterator(), false)
+                .collect(Collectors.toList());
     }
 }
