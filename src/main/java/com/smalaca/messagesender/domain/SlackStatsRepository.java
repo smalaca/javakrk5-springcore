3ebc0d09004e53d0e00b3aaac852fe76020f2f14
kamil <kbrzoza1994@gmail.com>
2018-03-10 14:40:57 +0100

    added new query to SlackStatsRepository.`

--- src/main/java/com/smalaca/messagesender/domain/SlackStatsRepository.java
+++ src/main/java/com/smalaca/messagesender/domain/SlackStatsRepository.java
@@ -2,9 +2,11 @@ package com.smalaca.messagesender.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
+
 
 public interface SlackStatsRepository extends CrudRepository<Stat, Integer> {
 
-    Optional<Stat> findAllByName(String from, String to);
+    Optional<List<Stat>> findFirst3ByMessageFromContainsAndMessageToContainsOrderByMessageToDesc(String messageFrom, String messageTo);
 }
--- src/test/java/com/smalaca/messagesender/domain/SlackStatsRepositoryTest.java
+++ src/test/java/com/smalaca/messagesender/domain/SlackStatsRepositoryTest.java
@@ -89,4 +89,5 @@ public class SlackStatsRepositoryTest {
         Assert.assertNull(slackStatsRepository.findOne(stat.getId()));
     }
 
+
 }
