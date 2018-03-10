b551b6996d4f28842d3260820a04a3c20926de99
Andrzej <andrzej.chudzicki@onet.pl>
2018-03-10 11:59:16 +0100

    StatsAgain added

---
+++ src/main/java/com/smalaca/messagesender/domain/Stat.java
@@ -0,0 +1,30 @@
+package com.smalaca.messagesender.domain;
+
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class Stat {
+
+    @Id
+    @GeneratedValue
+    private Integer statId;
+
+    private String messageFrom;
+    private String messageTo;
+    private String messageId;
+    private LocalDateTime date;
+
+    public Stat(String messageFrom, String messageTo, String messageId) {
+        this.messageFrom = messageFrom;
+        this.messageTo = messageTo;
+        this.messageId = messageId;
+        this.date = LocalDateTime.now();
+    }
+
+    public Integer getStatId() {
+        return statId;
+    }
+}
